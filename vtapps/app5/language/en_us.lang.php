590b44c356aa26d660e172132667f8a44dba0560
Joe Bordes <joe@tsolucio.com>
2011-12-21 10:50:16 +0000

    move graph tests to apps

---
+++ vtapps/app5/language/en_us.lang.php
@@ -0,0 +1,10 @@
+<?php
+$vtapps_strings = Array (
+'appName' => 'vtApp Demo Graph 1',
+'Title' => 'vtApp Demo Graph 1',
+'TooltipDescription' => 'Demo App with Graph',
+'Edit' => 'Edit',
+
+);
+
+?>
---
+++ vtapps/app6/language/en_us.lang.php
@@ -0,0 +1,10 @@
+<?php
+$vtapps_strings = Array (
+'appName' => 'vtApp Demo Graph 2',
+'Title' => 'vtApp Demo Graph 2',
+'TooltipDescription' => 'Demo App with Graph',
+'Edit' => 'Edit',
+
+);
+
+?>
